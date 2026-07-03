3359c3febbc12e6e15022fb984d1e1dcb24f8255
playvora <longliator.dev@gmail.com>
2026-07-03 22:49:01 +0700

    Update AudioCapture.cpp

--- WindowsServer/Src/AudioCapture.cpp
+++ WindowsServer/Src/AudioCapture.cpp
@@ -34,7 +34,7 @@ bool AudioCapture::Initialize() {
     Result = Client->GetMixFormat(&Format);
     if (FAILED(Result)) return false;
 
-    REFERENCE_TIME BufferDuration = 10000;
+    REFERENCE_TIME BufferDuration = 50000;
     Result = Client->Initialize(AUDCLNT_SHAREMODE_SHARED,
         AUDCLNT_STREAMFLAGS_LOOPBACK | AUDCLNT_STREAMFLAGS_EVENTCALLBACK,
         BufferDuration, 0, Format, nullptr);
